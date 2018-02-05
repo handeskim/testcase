eb754fb6d1feaa55ecfc0b4ffc443cba685cb394
handesk <handeskim@users.noreply.github.com>
2018-02-05 11:33:16 +0700

    add testcase

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+
+export class AppComponent {
+  title = 'app';
+  // logo = './public/logo.svg';
+  logo = "./public/logo.svg";
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
+})
+export class AppModule { }
